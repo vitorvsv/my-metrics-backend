1035ecfd33592241aba65914175f43964f4983e1
Vítor Soares Vian <vitorsoaresvian@gmail.com>
2025-04-13 21:13:21 -0300

    Feature targets (#3)
    * feat(targets): Added route to create a new target
    
    * feat(targets): Adjust test to send string and added client.rest file to make requests and document the REST API

---
+++ src/application/usecases/CreateTargetUseCase.ts
@@ -0,0 +1,36 @@
+import { TargetRepository } from '../../infra/repositories/TargetRepository';
+import TargetEntity from '../../domain/entities/TargetEntity';
+
+export default class CreateTargetUseCase {
+    constructor(private readonly targetRepository: TargetRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+        const target = TargetEntity.create(
+            input.description,
+            input.frequency,
+            input.value,
+            input.startDate,
+            input.endDate,
+            input.status,
+            input.accountId,
+        );
+        const targetCreated = await this.targetRepository.createTarget(target);
+        return {
+            targetId: targetCreated.getTargetId().getValue(),
+        };
+    }
+}
+
+type Input = {
+    description: string;
+    frequency: string;
+    value: number;
+    startDate: Date;
+    endDate: Date;
+    status: string;
+    accountId: string;
+};
+
+type Output = {
+    targetId: string;
+};
